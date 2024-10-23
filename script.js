a0f1222cfd67401dbf1d2d3106a4d87a377286c2
HerculesOl <147669122+HerculesOl@users.noreply.github.com>
2024-10-23 20:15:43 -0300

    Update script.js

--- script.js
+++ script.js
@@ -3,14 +3,14 @@ let titulo = document.querySelector("h1")
 let fundo = document.querySelector("body")
 let img = document.querySelector("img")
 
-let clickTitulo = 0
+let tituloAlter = 0
 let  alterarTitulo = () => {
     let titulo = document.querySelector('h1')
-if(clickTitulo == 1){
+if(tituloAlter == 1){
     titulo.innerText = "Fernando Beira mar é preso"
-    clickTitulo =0
+    tituloAlter =0
 }else{
-    clickTitulo=1
+    tituloAlter=1
 titulo.innerText = "Fernandinho Beira mar e solto"
 }
 }
